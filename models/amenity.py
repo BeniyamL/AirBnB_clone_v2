705af5ab7c97c9dbed887adaacec4e9252c0d05d
Beniyam Legesse <lbeniyam@gmail.com>
2021-08-10 04:16:09 -0700

    fix on tablename position

--- models/amenity.py
+++ models/amenity.py
@@ -8,9 +8,8 @@ from os import getenv
 
 class Amenity(BaseModel, Base):
     """ class definition for Amenity class """
-
+    __tablename__ = 'amenities'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
         place_amenities = relationship('Place', secondary='place_amenity',
                                        viewonly=False)
